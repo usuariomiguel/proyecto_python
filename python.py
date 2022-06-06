bab480af2d14de1056936eda0c375857955f587f
MiguelAngel <ansonfull@gmail.com>
2022-06-06 23:55:42 +0200

    commit

--- python.py
+++ python.py
@@ -5,8 +5,10 @@ import MySQLdb
 os.system("clear")
 conexion_db=MySQLdb.connect(host="localhost",user="academia",passwd="academia",db="Exameness")
 cursor=conexion_db.cursor()
+
 def emusica():
 	os.system("clear")
+	cont = 0
 	print ("Test de Musica")
 	for a in range(1,11):
 		select_preg = "SELECT * FROM musica WHERE idexamen1 = %(id)s"
@@ -17,14 +19,21 @@ def emusica():
 		select_resp = "SELECT * FROM resp_musica WHERE idmusica = %(id)s"
 		cursor.execute(select_resp, { 'id': a })
 		for respuestas in cursor.fetchall():
-			print (respuestas[1],sep=".")
+			print (respuestas[4],respuestas[1],sep=")")
 		respuesta_musica = input("Solucion: ")
-		
-		print (" ")
+		apartado = "SELECT * FROM resp_musica WHERE idmusica = %(id)s and abcd = %(apartado)s"
+		cursor.execute(apartado, { 'id': a, 'apartado': respuesta_musica })
+		consulta = cursor.fetchone()
+		if consulta[3] == 1:
+			cont = cont + 1
+	print ("------------------------")
+	print ("Tu nota es",cont,sep=": ")
+	print (" ")
 	# ~ conexion_db.close()
-	volver = input(" ")
+	input(" ")
 def enatacion():
 	os.system("clear")
+	cont = 0
 	print ("Test de Natacion")
 	for a in range(1,10):
 		select_preg = "SELECT * FROM natacion WHERE idnatacion = %(id)s"
@@ -35,14 +44,20 @@ def enatacion():
 		select_resp = "SELECT * FROM resp_natacion WHERE id_respuestas = %(id)s"
 		cursor.execute(select_resp, { 'id': a })
 		for respuestas in cursor.fetchall():
-			print (respuestas[0],respuestas[1],sep=")")
+			print (respuestas[4],respuestas[1],sep=")")
 		respuesta_natacion = input("Solucion: ")
-		#if respuesta_natacion == 1:
-		print (" ")
+		apartado = "SELECT * FROM resp_natacion WHERE id_respuestas = %(id)s and abcd = %(apartado)s"
+		cursor.execute(apartado, { 'id': a, 'apartado': respuesta_natacion })
+		consulta = cursor.fetchone()
+		if consulta[3] == 1:
+			cont = cont + 1
+	print ("------------------------")
+	print ("Tu nota es",cont,sep=": ")
 	# ~ conexion_db.close()
-	volver = input(" ")
+	input(" ")
 def eprogramacion():
 	os.system("clear")
+	cont = 0
 	print ("Test de Programacion")
 	for a in range(1,10):
 		select_preg = "SELECT * FROM programacion WHERE idexamen1 = %(id)s"
@@ -53,13 +68,20 @@ def eprogramacion():
 		select_resp = "SELECT * FROM resp_programacion WHERE ip_resp = %(id)s"
 		cursor.execute(select_resp, { 'id': a })
 		for respuestas in cursor.fetchall():
-			print (respuestas[1])
+			print (respuestas[4],respuestas[1],sep=")")
 		respuesta_programacion = input("Solucion: ")
-		print (" ")
+		apartado = "SELECT * FROM resp_programacion WHERE ip_resp = %(id)s and apartado = %(abcd)s"
+		cursor.execute(apartado, { 'id': a, 'abcd': respuesta_programacion })
+		consulta = cursor.fetchone()
+		if consulta[3] == 1:
+			cont = cont + 1
+	print ("------------------------")
+	print ("Tu nota es",cont,sep=": ")
 	# ~ conexion_db.close()
-	volver = input(" ")
+	input(" ")
 def eturismo():
 	os.system("clear")
+	cont = 0
 	print ("Test de Turismo")
 	for a in range(1,10):
 		select_preg = "SELECT * FROM turismo WHERE idexamen1 = %(id)s"
@@ -70,13 +92,20 @@ def eturismo():
 		select_resp = "SELECT * FROM resp_turismo WHERE idresp_turismo = %(id)s"
 		cursor.execute(select_resp, { 'id': a })
 		for respuestas in cursor.fetchall():
-			print (respuestas[1])
-		respuesta_turismo = input("Solucion: ")
-		print (" ")
+			print (respuestas[4],respuestas[1],sep=")")
+		respuesta_turismo = input("Solucion: ")		
+		apartado = "SELECT * FROM resp_turismo WHERE idresp_turismo = %(id)s and apartado = %(abcd)s"
+		cursor.execute(apartado, { 'id': a, 'abcd': respuesta_turismo })
+		consulta = cursor.fetchone()
+		if consulta[3] == 1:
+			cont = cont + 1
+	print ("------------------------")
+	print ("Tu nota es",cont,sep=": ")
 	# ~ conexion_db.close()
-	volver = input(" ")
+	input(" ")
 op=0
 while op!=5:
+	os.system("clear")
 	print ("Realizaremos un examen")
 	print ("")
 	print ("1. Musica")
